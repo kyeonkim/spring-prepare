6116ae0f2ad606392151d90444328454cd9b60e2
kyeonkim <kyeonkim5021@gmail.com>
2024-07-27 11:48:45 +0900

    RESTfulAPI 만들어보기 - 요청 경로가 같아도 http method는 중복되지 않기에 method에 맞게 요청을 처리할 수 있다.

--- src/main/java/com/sparta/springprepare/controller/HelloController.java
+++ src/main/java/com/sparta/springprepare/controller/HelloController.java
@@ -1,13 +1,32 @@
 package com.sparta.springprepare.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
 public class HelloController {
 
-    @GetMapping("/api/hello")
-    public String hello() {
-        return "Hello World!";
+    @GetMapping()
+    @ResponseBody
+    public String get() {
+        return "GET Method Request";
+    }
+
+    @PostMapping()
+    @ResponseBody
+    public String post() {
+        return "Post Method Request";
+    }
+
+    @PutMapping()
+    @ResponseBody
+    public String put() {
+        return "Put Method Request";
+    }
+
+    @DeleteMapping()
+    @ResponseBody
+    public String delete() {
+        return "Delete Method Request";
     }
 }
