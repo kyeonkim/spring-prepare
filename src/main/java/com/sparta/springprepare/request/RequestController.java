2f9d9093553ebab5efd20b88d5f620f30006d9e5
kyeonkim <kyeonkim5021@gmail.com>
2024-07-27 16:52:39 +0900

    Request 방식 중 Path Variable 와 Param, PostParam을 사용했을 때 데이터가 어떻게 넘어오는지 확인

---
+++ src/main/java/com/sparta/springprepare/request/RequestController.java
@@ -0,0 +1,32 @@
+package com.sparta.springprepare.request;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/hello/request")
+public class RequestController {
+
+    @GetMapping("/form/html")
+    public String helloForm() {
+        return "hello-request-form";
+    }
+
+    @GetMapping("/star/{name}/age/{age}")
+    @ResponseBody
+    public String helloRequestPath(@PathVariable String name, @PathVariable int age) {
+        return String.format("Hello, @PathVariable.<br> name = %s, age = %d", name, age);
+    }
+
+    @GetMapping("/form/param")
+    @ResponseBody
+    public String helloGetRequestParam(@RequestParam String name, @RequestParam int age) {
+        return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
+    }
+
+    @PostMapping("/form/param")
+    @ResponseBody
+    public String helloPostRequestParam(@RequestParam String name, @RequestParam int age) {
+        return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
+    }
+}
