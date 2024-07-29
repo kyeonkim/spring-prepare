aa8fcfae4eb2336c2f66a0624ee51c36fcbe2ec8
kyeonkim <kyeonkim5021@gmail.com>
2024-07-30 02:49:07 +0900

    Query Methods를 이용하여 원하는 데이터를 뽑는 메서드 생성하기

--- src/main/java/com/sparta/springprepare/repository/MemoRepository.java
+++ src/main/java/com/sparta/springprepare/repository/MemoRepository.java
@@ -3,4 +3,9 @@ package com.sparta.springprepare.repository;
 import com.sparta.springprepare.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemoRepository extends JpaRepository<Memo, Long> { }
+import java.util.List;
+
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+    List<Memo> findAllByOrderByModifiedAtDesc();
+    List<Memo> findAllByUsername(String username);
+}
